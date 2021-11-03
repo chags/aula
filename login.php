e13823b9c472b488d34b47e9b7557271168773c6
junior sousa <92957053+juniors0101@users.noreply.github.com>
2021-11-03 14:59:18 -0300

    update
    update login

--- login.php
+++ login.php
@@ -6,22 +6,32 @@ $title = "<h1>Login</h1><br>";
 echo $title;
 #aula de criar pagina de login
 ?>
-<form method="POST" action="logando.php">
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text"> Nunca compartilharemos seu e-mail com mais ninguém.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Senha</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <button type="submit" class="btn btn-primary">Enviar</button>
-</form>
-
-
-
-
+<div class="container">
+    <div class="row align-items-start">
+          <div class="col-sm-2">
+          
+          </div>
+          <div class="col-sm-8">
+            <form method="POST" action="logando.php">
+              <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text"> Nunca compartilharemos seu e-mail com mais ninguém.</div>
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Senha</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+              </div>
+              <button type="submit" class="btn btn-primary">Enviar</button>
+            </form>
+         
+          </div>
+      <div class="col-sm-2">
+        
+      </div>
+    </div>
+</div>
+</div>
 <?php
 include_once ("./componentes/footer.php");
 ?>
