044f46b5fb51b8e68e217d374e47cc1b34349aaf
junior sousa <92957053+juniors0101@users.noreply.github.com>
2021-11-15 15:58:25 -0300

    update inicio
    update

--- index.php
+++ index.php
@@ -28,44 +28,68 @@ include_once ("./componentes/navbar.php");
         <div class="col">
         <h1>Bem Vindo ao site explicativo PHP</h1><br>
 
-
-  <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-  O que é PHP e para que serve?
-  </button>
-</p>
-<div class="collapse" id="collapseExample">
-  <div class="card card-body">
-  <p> O PHP (um acrônimo recursivo para PHP: Hypertext Preprocessor ) é uma linguagem de script 
-open source de uso geral, muito utilizada, e especialmente adequada para o desenvolvimento 
-web e que pode ser embutida dentro do HTML.</p> 
+<div class="accordion" id="accordionPanelsStayOpenExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+      O que é PHP e para que serve?
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+      <div class="accordion-body">
+        <strong>O PHP (um acrônimo recursivo para PHP: Hypertext Preprocessor ) é uma linguagem de script 
+                open source de uso geral, muito utilizada, e especialmente adequada para o desenvolvimento 
+                web e que pode ser embutida dentro do HTML.</strong>
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+      Para que é usado o PHP?
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+      <div class="accordion-body">
+        <strong>O PHP pode ser utilizado na maioria dos sistemas operacionais, incluindo Linux, várias 
+        variantes do Unix (como HP-UX, Solaris e OpenBSD), Microsoft Windows, macOS, RISC OS e 
+        provavelmente outros. O PHP também tem suporte à maioria dos servidores web atualmente. 
+        Isso inclui o Apache, o IIS e muitos outros.</strong> 
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+      Como funciona a linguagem PHP?
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+      <div class="accordion-body">
+        <strong>A princípio, a linguagem PHP é uma linguagem livre, sem taxas e custos na instalação 
+                de sistemas operacionais, possibilitando a interação por meio de dados e aplicações 
+                presentes no servidor. A linguagem de scripts também é usada em união com o HTML, 
+                a linguagem de marcação utilizada na construção de páginas Web.</strong>
+        </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+      Onde e como a linguagem PHP é mais utilizada?
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+      <div class="accordion-body">
+        <strong>Usado majoritariamente em aplicações web, a linguagem PHP é útil para incluir funções
+        a uma página que o HTML não é capaz de suportar. A linguagem também é utilizada para
+        integração entre informações de sua página e banco de dados MySQL, por exemplo. 
+        Sites como o Yahoo e a versão web do Facebook são mantidas em PHP.</strong>
+      </div>
+    </div>
   </div>
 </div>
 
-
-
-
-        <h5>O que é PHP e para que serve?</h5><br>
-<p> O PHP (um acrônimo recursivo para PHP: Hypertext Preprocessor ) é uma linguagem de script 
-open source de uso geral, muito utilizada, e especialmente adequada para o desenvolvimento 
-web e que pode ser embutida dentro do HTML.</p> 
-
-<h5>Para que é usado o PHP?</h5> <br>
-<p>O PHP pode ser utilizado na maioria dos sistemas operacionais, incluindo Linux, várias 
-variantes do Unix (como HP-UX, Solaris e OpenBSD), Microsoft Windows, macOS, RISC OS e 
-provavelmente outros. O PHP também tem suporte à maioria dos servidores web atualmente. 
-Isso inclui o Apache, o IIS e muitos outros.</p> 
-
-<h5>Como funciona a linguagem PHP?</h5><br>
-<p>A princípio, a linguagem PHP é uma linguagem livre, sem taxas e custos na instalação 
-de sistemas operacionais, possibilitando a interação por meio de dados e aplicações 
-presentes no servidor. A linguagem de scripts também é usada em união com o HTML, 
-a linguagem de marcação utilizada na construção de páginas Web. </p> 
-
-<h5>Onde e como a linguagem PHP é mais utilizada?</h5><br>
-<p>Usado majoritariamente em aplicações web, a linguagem PHP é útil para incluir funções
-a uma página que o HTML não é capaz de suportar. A linguagem também é utilizada para
-integração entre informações de sua página e banco de dados MySQL, por exemplo. 
-Sites como o Yahoo e a versão web do Facebook são mantidas em PHP.</p> 
         </div>
     </div>
     <div class="row">
